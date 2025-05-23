09c388f0965d9df855c2002ec82b992917accad7
Mehedi Hasan <dev.hasan10@gmail.com>
2025-05-24 02:26:47 +0600

    7th commit

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ async function run() {
       res.send(result);
     });
 
-    // : Delete plant by ID
+    //  Delete plant by ID
     app.delete("/plants/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
